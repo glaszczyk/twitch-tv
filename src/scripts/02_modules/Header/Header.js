304336ccf8059cd70dfa08c59dcf8a206b34f984
Grzegorz Laszczyk <glas@softserveinc.com>
2017-06-09 15:27:46 +0200

    Fetch top videos from Twitch TV

--- src/scripts/02_modules/Header/Header.js
+++ src/scripts/02_modules/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 import './Header.scss';
 import Button from '../../01_components/Button/Button';
 import Input from '../../01_components/Input/Input';
@@ -6,12 +7,38 @@ import Input from '../../01_components/Input/Input';
 class Header extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			topVideoList: null
+		}
 		this.onClickAll = this.onClickAll.bind(this);
 		this.onClickOnline = this.onClickOnline.bind(this);
 		this.onClickOffline = this.onClickOffline.bind(this);
 	}
+
+	componentWillMount() {
+		const url = "https://api.twitch.tv/kraken/videos/top"
+		fetch(
+			url,
+			{
+				method: 'GET',
+				headers: {
+					"Client-ID": "my05o002gwlb9y2l520uwuwpo312vix",
+					"Accept": "application/vnd.twitchtv.v5+json"
+				},
+				credentials: "same-origin"
+			})
+			.then(response => response.json())
+			.then(json => {
+				this.setState({
+					topVideoList: json.vods
+				})
+			}
+		)
+	}
 	onClickAll() {
-		console.log('All users');
+		const { topVideoList } = this.state;
+		topVideoList.map( video => console.log(video) );
+
 	}
 	onClickOnline() {
 		console.log('Online users');
